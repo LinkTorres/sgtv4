f9e444ea4c5af7d221c985ab029f86c8dd633a96
LinkTorres <et_3001@hotmail.com>
2015-06-29 20:55:01 -0500

    ok

--- app/routes.php
+++ app/routes.php
@@ -52,52 +52,53 @@ Route::get('generar', function()
 	        						->join('Profesor','Profesor.Cedula','=','Jurado.Profesor_Cedula')
 	        						->join('users', 'users.id', '=', 'Profesor.Usuario_id_Usuario')
 	        						->where('TT_Num_TT','=',$idTT)->where('Participacion','Director')
-	        						->select('Nombre','ApellidoP','ApellidoM')->get()[0];
+	        						->pluck('Nombre')[0];
 	        $director2=DB::table('Jurado')
 	        						->join('Profesor','Profesor.Cedula','=','Jurado.Profesor_Cedula')
 	        						->join('users', 'users.id', '=', 'Profesor.Usuario_id_Usuario')
 	        						->where('TT_Num_TT','=',$idTT)->where('Participacion','Director')
-	        						->select('Nombre','ApellidoP','ApellidoM')->get()[1];
+	        						->pluck('Nombre')[1];
 
 	        $sinodal1=DB::table('Jurado')->join('Profesor','Profesor.Cedula','=','Jurado.Profesor_Cedula')
 	        						->join('users', 'users.id', '=', 'Profesor.Usuario_id_Usuario')
 	        						->where('TT_Num_TT','=',$idTT)->where('Participacion','Sinodal')
-	        						->select('Nombre','ApellidoP','ApellidoM')->get()[0];
+	        						->pluck('Nombre')[0];
 
 	        $sinodal2=DB::table('Jurado')->join('Profesor','Profesor.Cedula','=','Jurado.Profesor_Cedula')
 	        						->join('users', 'users.id', '=', 'Profesor.Usuario_id_Usuario')
 	        						->where('TT_Num_TT','=',$idTT)->where('Participacion','Sinodal')
-	        						->select('Nombre','ApellidoP','ApellidoM')->get()[1];
+	        						->pluck('Nombre')[1];
 
 	        $sinodal3=DB::table('Jurado')->join('Profesor','Profesor.Cedula','=','Jurado.Profesor_Cedula')
 	        						->join('users', 'users.id', '=', 'Profesor.Usuario_id_Usuario')
 	        						->where('TT_Num_TT','=',$idTT)->where('Participacion','Sinodal')
-	        						->select('Nombre','ApellidoP','ApellidoM')->get()[2];
+	        						->pluck('Nombre')[2];
+
 	        $cedulaDirector=DB::table('Jurado')
 	        						->join('Profesor','Profesor.Cedula','=','Jurado.Profesor_Cedula')
 	        						->join('users', 'users.id', '=', 'Profesor.Usuario_id_Usuario')
 	        						->where('TT_Num_TT','=',$idTT)->where('Participacion','Director')
-	        						->select('Cedula')->get()[0];
+	        						->pluck('Cedula')[0];
 	        $cedulaDirector2=DB::table('Jurado')
 	        						->join('Profesor','Profesor.Cedula','=','Jurado.Profesor_Cedula')
 	        						->join('users', 'users.id', '=', 'Profesor.Usuario_id_Usuario')
 	        						->where('TT_Num_TT','=',$idTT)->where('Participacion','Director')
-	        						->select('Cedula')->get()[1];
+	        						->pluck('Cedula')[1];
 
 	        $cedulaSinodal1=DB::table('Jurado')->join('Profesor','Profesor.Cedula','=','Jurado.Profesor_Cedula')
 	        						->join('users', 'users.id', '=', 'Profesor.Usuario_id_Usuario')
 	        						->where('TT_Num_TT','=',$idTT)->where('Participacion','Sinodal')
-	        						->select('Cedula')->get()[0];
+	        						->pluck('Cedula')[0];
 
 	        $cedulaSinodal2=DB::table('Jurado')->join('Profesor','Profesor.Cedula','=','Jurado.Profesor_Cedula')
 	        						->join('users', 'users.id', '=', 'Profesor.Usuario_id_Usuario')
 	        						->where('TT_Num_TT','=',$idTT)->where('Participacion','Sinodal')
-	        						->select('Cedula')->get()[1];
+	        						->pluck('Cedula')[1];
 
 	        $cedulaSinodal3=DB::table('Jurado')->join('Profesor','Profesor.Cedula','=','Jurado.Profesor_Cedula')
 	        						->join('users', 'users.id', '=', 'Profesor.Usuario_id_Usuario')
 	        						->where('TT_Num_TT','=',$idTT)->where('Participacion','Sinodal')
-	        						->select('Cedula')->get()[2];
+	        						->pluck('Cedula')[2];
 
 	        $opcion=DB::table('Pasante')
 	        				->join('users','users.id','=','Pasante.Usuario_id_Usuario')
@@ -127,7 +128,7 @@ Route::get('generar', function()
 	        				->where('Boleta','=',$boleta)
 	        				->pluck('Acta_Folio');
 
-			$html= View::make('acta/acta2',$Profes)
+			/*$html= View::make('acta/acta2',$Profes)
 			->with('Profes',$Profes)
 			->with('hora_inicio',$hora_inicio)
 			->with('dia',$fecha)
@@ -151,7 +152,33 @@ Route::get('generar', function()
 			->with('cedulaDirector2',$cedulaDirector2)
 			->with('cedulaSinodal1',$cedulaSinodal1)
 			->with('cedulaSinodal2',$cedulaSinodal2)
-			->with('cedulaSinodal3',$cedulaSinodal3);
+			->with('cedulaSinodal3',$cedulaSinodal3);*/
+
+			$html= View::make('acta/acta2',$Profes)
+			->with('Profes',$Profes)
+			->with('hora_inicio',"$hora_inicio")
+			->with('dia',"$fecha")
+		    ->with('mes',"$fecha")
+			->with('anio',"$fecha")
+			->with('director',"director")
+			->with('director2',"director2")
+			->with('sinodal1',"sinodal1")
+			->with('sinodal2',"sinodal2")
+			->with('sinodal3',"sinodal3")
+			->with('opcion',"$opcion")
+			->with('carrera',"$carrera")
+			->with('nombre',"$nombre")
+			->with('apellidoP',"$apellidoP")
+			->with('apellidoM',"$apellidoM")
+			->with('genero',"$genero")
+			->with('hora_fin',"$hora_fin")
+			->with('boleta',"$boleta")
+			->with('acta',"$acta")
+			->with('cedulaDirector',"$cedulaDirector")
+			->with('cedulaDirector2',"$cedulaDirector2")
+			->with('cedulaSinodal1',"$cedulaSinodal1")
+			->with('cedulaSinodal2',"$cedulaSinodal2")
+			->with('cedulaSinodal3',"$cedulaSinodal3");
 
 			//$html = View::make('GestionarProfesores')->with(array('Profes'=>$Profes));
 			return PDF::load($html, 'Letter', 'portrait')->show();
